7b216b9b6cb0fd5a6542c4100d5f9a800cdd1468
George <64893104+geckoro@users.noreply.github.com>
2021-04-29 20:09:02 +0300

    Created a home window, where the client is prompted to enter a name.

---
+++ ChatClient/ChatWPF/Models/Label.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatWPF.Models
+{
+    public class Label : INotifyPropertyChanged
+    {
+        private string message;
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+                NotifyPropertyChanged("Message");
+            }
+        }
+
+        public Label(string message)
+        {
+            Message = message;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ChatClient/ChatWPF/ViewModels/HomeVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatWPF.Models;
+
+namespace ChatWPF.ViewModels
+{
+    class HomeVM
+    {
+        public Label StatusLabel { get; set; }
+
+        public HomeVM()
+        {
+            StatusLabel = new Label("Please enter your desired name");
+        }
+    }
+}
