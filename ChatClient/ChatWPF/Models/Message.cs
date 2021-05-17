0fb1a3d2128fd64bfbbecdd86e8ff3f1f8936d94
MisuZ99 <chromezephyr99@gmail.com>
2021-05-17 18:38:05 +0300

    Added a class and methods to make a text block from a given string

---
+++ ChatClient/ChatWPF/Models/Message.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatWPF.Models
+{
+    class Message : INotifyPropertyChanged
+    {
+        private string _outputMessage;
+
+        public string OutputMessage
+        {
+            get { return _outputMessage; }
+            set
+            {
+                _outputMessage = value;
+                OnPropertyChanged("OutputMessage");
+            }
+        }
+
+        public Message(string message)
+        {
+            string finalMessage = "";
+            string[] words = message.Split();
+            foreach (var word in words)
+            {
+                if (word[0] == '*' && word[word.Length - 1] == '*')
+                {
+                    finalMessage = finalMessage + " " + "<Bold>" + word.Substring(1,word.Length-2) + "</Bold>";
+                }
+                else if(word[0] == '_' && word[word.Length - 1] == '_')
+                {
+                    finalMessage = finalMessage + " " + "<Italic>" + word.Substring(1, word.Length - 2) + "</Italic>";
+                }
+                else if (word[0] == '~' && word[word.Length - 1] == '~')
+                {
+                    finalMessage = finalMessage + " " + "<Italic>" + word.Substring(1, word.Length - 2) + "</Italic>";
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
