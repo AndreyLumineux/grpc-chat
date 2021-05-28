0edee06ca5f38233ab0ce5626360fbc68aa39110
OvertakerQ <andrey0103@yahoo.com>
2021-05-28 10:07:58 +0300

    Modified Operations class into a singleton.

--- ChatClient/ChatWPF/ViewModels/ChatVM.cs
+++ ChatClient/ChatWPF/ViewModels/ChatVM.cs
@@ -1,67 +1,63 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
+﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
-using System.Windows.Threading;
 using ChatWPF.Commands;
 using ChatWPF.Models;
 using ChatWPF.Services;
-using Meziantou.Framework.WPF.Collections;
 
 namespace ChatWPF.ViewModels
 {
-    public class ChatVM : BaseVM
-    {
-        private readonly Operations _operations;
-        private readonly object _lock = new();
+	public class ChatVM : BaseVM
+	{
+		private readonly object _lock = new();
 
-        public ObservableCollection<string> Messages { get; } = new();
+		public ObservableCollection<string> Messages { get; } = new();
 
-        public ClientsList Clients { get; set; }
-        public Input InputBox { get; set; }
+		public ClientsList Clients { get; set; }
+		public Input InputBox { get; set; }
 
-        public ChatVM()
-        {
-            _operations = new Operations(this, MainVM._navigationStore);
-            var clientsList = _operations.GetAllClients();
+		public ChatVM()
+		{
+			var clientsList = Operations.Instance.GetAllClients();
 
-            Clients = new ClientsList();
-            foreach (var item in clientsList)
-            {
-                Clients.AddClient(item);
-            }
+			Clients = new ClientsList();
 
-            InputBox = new Input();
-            Messages.Add("Yest");
-        }
+			foreach (var item in clientsList)
+			{
+				Clients.AddClient(item);
+			}
 
-        private ICommand _sendPressed;
-        public ICommand SendPressed
-        {
-            get
-            {
-                if (_sendPressed == null)
-                {
-                    _sendPressed = new RelayCommand(async param => await _operations.Send(InputBox.InputMessage));
-                }
-                return _sendPressed;
-            }
-        }
+			InputBox = new Input();
+			Messages.Add("Yest");
+		}
 
-        private ICommand _sendVoid;
-        public ICommand SendVoid
-        {
-            get
-            {
-                if (_sendVoid == null)
-                {
-                    _sendVoid = new RelayCommand(_operations.SendVoid);
-                }
-                return _sendVoid;
-            }
-        }
-    }
+		private ICommand _sendPressed;
+
+		public ICommand SendPressed
+		{
+			get
+			{
+				if (_sendPressed == null)
+				{
+					_sendPressed = new RelayCommand(async param => await Operations.Instance.Send(InputBox.InputMessage));
+				}
+
+				return _sendPressed;
+			}
+		}
+
+		private ICommand _sendVoid;
+
+		public ICommand SendVoid
+		{
+			get
+			{
+				if (_sendVoid == null)
+				{
+					_sendVoid = new RelayCommand(Operations.Instance.SendVoid);
+				}
+
+				return _sendVoid;
+			}
+		}
+	}
 }
